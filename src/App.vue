932972649d98e84ab36d1095708aecf2ef9ddd98
thinhphungdev <phungthinh0946519356@gmail.com>
2020-10-07 15:47:05 +0700

    push all file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="container">
+      <h1> The Thinh Phung's Todo Manager App</h1>
+        <AddToDo />
+        <FilterTodo />
+        <Todos />
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos"
+import AddToDo from "./components/AddToDo"
+import FilterTodo from "./components/FilterTodo"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos,
+    AddToDo,
+    FilterTodo
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+    line-height: 1.6;
+    background: #e8f7f0;
+  }
+
+  .container {
+    max-width: 1100px;
+    margin: auto;
+    overflow: auto;
+    padding: 0 2rem;
+  }
+
+  .container h1 {
+    text-align: center;
+  }
 </style>
---
+++ src/components/AddToDo.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <h3>Add Todo </h3>
+        <div class="add">
+            <form @submit = "onSubmit" >
+                <input type="text" v-model="title" placeholder="add to do...">
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex';
+
+export default {
+    name: 'AddTodo',
+
+    data() {
+        return {
+            title: ''
+        }
+    },
+
+    methods: {
+        ...mapActions([
+            'addTodo'
+        ]),
+
+        onSubmit(e) {
+            e.preventDefault(); // make sure this event doesnt make our form to rerender itself 
+            this.addTodo(this.title)
+        }
+    }
+} 
+
+</script>
+
+<style scoped>
+    form {
+    display: flex;
+    }
+
+    input[type="text"] {
+    flex: 10;
+    padding: 10px;
+    border: 1px solid #41b883;
+    outline: 0;
+    }
+
+    input[type="submit"] {
+    flex: 2;
+    background: #41b883;
+    color: #fff;
+    border: 1px #41b883 solid;
+    cursor: pointer;
+    }
+</style>
